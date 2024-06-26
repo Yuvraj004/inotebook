7f85df0a67ae3c93dbe151fe9b32ea274847813f
Yuvraj004 <yuvrajchat@gmail.com>
2024-06-26 23:00:56 +0530

    Changes for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 
 const mongoose =require('mongoose');//importing mongoose functon from mongoose library used for editing mongodb
-const mongoURI="mongodb+srv://yuvrajchat:IJUSWzRDesGiMYv2@cluster0.w6hbhbu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongoURI=process.env.MONGO_URI;
 //connecting while creating a datbase locally
 
 //connectto mongo function to check whether database is connected or not
